488527b53e79ef6ef1bdf7e0efcbfec961487a61
rodennis <rev.rltodd@gmail.com>
2021-09-09 11:03:48 -0500

    add index.js

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-let pick = ["rock", "paper", "scissors"]
 
-function computerPlay() {
-    let compChoice = pick[ Math.floor(Math.random() * pick.length) ]
-    return compChoice
-}     
-function playerSelection() { 
-    let playChoice = ("")
-}
+    let pick = ["rock", "paper", "scissors"]
+    let result;
 
-prompt("make a selection rock, paper, or scissors?")
-
-if (playerSelection() === computerPlay()){
-    alert ("It's a tie!")
-} 
-else if (playerSelection() === "scissors" && computerPlay() === "rock") {
-    
-    alert ("Rock beats Scissors! Computer wins");
-}
-else if (playerSelection() === "rock" && computerPlay() === "scissors") {
-   
-    alert ("Rock beats Scissors! Player wins")
-}
-else if (playerSelection() === "rock" && computerPlay() === "paper") {
-    
-    alert ("Paper beats Rock! Computer wins")
-}
-else if (playerSelection() === "paper" && computerPlay() === "rock") {
-   
-    alert ("Paper beats Rock! Player wins")
-}
-else if (playerSelection() === "paper" && computerPlay() === "scissors") {
-    
-    alert ("Scissors beats Paper! Computer wins");
-}
-else if (playerSelection() === "scissors" && computerPlay() === "paper") {
-   
-    alert ("Scissors beats Paper! Player wins");
-}
-
+    function computerPlay() {
+        let compChoice = pick[ Math.floor(Math.random() * pick.length) ]
+        return compChoice
+    }
+    function playerSelection() {
+        let playChoice = ("")
+    }       
+        let playChoice = prompt("make a selection rock, paper, scissors?")
+    }
+        if (playerSelection() === computerPlay()) {
+            alert ("its a draw")
+        }
+        else if (computerPlay() === "rock" && playerSelection() === "scissors" || computerPlay() === "scissors" && playerSelection() === "paper" || computerPlay() === "paper" && playerSelection() === "rock") {
+            alert ("Computer has won!");
+        }
+        else if (playerSelection() === "rock" && computerPlay() === "scissors" || playerSelection() === "scissors" && computerPlay() === "paper" || playerSelection() === "paper" && computerPlay() === "rock"); {
+            alert ("player has won!")
+        }
+       
     
