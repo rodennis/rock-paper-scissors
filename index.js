0a1f86e07acb7eaed2b27c182bea8fb068c86662
rodennis <rev.rltodd@gmail.com>
2021-09-10 10:25:39 -0500

    add index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@
         return compChoice
     }
     function playerSelection() {
-        let playChoice = ("")
-    }       
-        let playChoice = prompt("make a selection rock, paper, scissors?")
+        let playChoice = ["rock", "paper", "scissors"]
+           return playChoice
+    }
+            prompt("make a selection rock, paper, or scissors?")
 
     function playRound(playerSelection, computerPlay) {
 
@@ -16,21 +17,18 @@
             alert ("its a draw")
         }
         else if (
-            (computerPlay() === "rock" && playerSelection() === "scissors") || 
-            (computerPlay() === "scissors" && playerSelection() === "paper") || 
-            (computerPlay() === "paper" && playerSelection() === "rock")
-            )
-         {
-            alert ("Computer has won!");
+            computerPlay() === "rock" && playerSelection() === "scissors" || computerPlay() === "scissors" && playerSelection() === "paper" || computerPlay() === "paper" && playerSelection() === "rock"
+            ) 
+            {
+            alert ("Computer has won!")
         }
         else if (
-            (playerSelection() === "rock" && computerPlay() === "scissors") || 
-            (playerSelection() === "scissors" && computerPlay() === "paper") || 
-            (playerSelection() === "paper" && computerPlay() === "rock")
-            ) 
+            playerSelection() === "rock" && computerPlay() === "scissors" || playerSelection() === "scissors" && computerPlay() === "paper" || playerSelection() === "paper" && computerPlay() === "rock"
+            )
             {
             alert ("player has won!")
         }
+        playRound()
     }
        
     
