74dc7e2d42c55134fe4077077b8a2984f6abfb64
rodennis <rev.rltodd@gmail.com>
2021-09-14 18:37:34 -0500

    add galactic.jpeg and index.css and index.html and index.js and paper1.png and rock-removebg-preview.png and scissors-removebg-preview.png

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 let pick = ["rock", "paper", "scissors"];
 let compChoice = pick[ Math.floor(Math.random() * pick.length)]
+let playSelect = ["rock", "paper", "scissors"]
 
-let playSelect = window.prompt ("Choose your weapon rock, paper, or scissors?")
+//let playSelect =  window.prompt("Choose your weapon rock, paper, or scissors?")//
+
+const btn0 = document.querySelector("#rock");
+btn0.addEventListener('click', () => {
+  if (playSelect === "rock") {
+    startRound()
+  }
+})
+
+  const btn1 = document.querySelector("#paper");
+btn1.addEventListener('click', () => {
+  if (playSelect === "paper") {
+    startRound()
+  }
+})
+  
+  const btn2 = document.querySelector("#scissors");
+btn2.addEventListener('click', () => {
+  if (playSelect === "scissors") {
+    startRound()
+  }
+})  
 
 function startRound() {
 
@@ -26,7 +48,10 @@ function startRound() {
     }
 }
 
+
 startRound()
+
+
 
 
 
